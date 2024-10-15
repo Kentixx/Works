c43f60ebdaf0730874e03b4e4dfbe9f9f108e530
Kentixx <hovanskiyivan2@gmail.com>
2024-10-15 14:59:05 +0300

    fixed 1 lab

--- 1/1.cpp
+++ 1/1.cpp
@@ -15,20 +15,18 @@ int main() {
 	std::cin >> k;
 	std::cout << "precision: ";
 	std::cin >> prec;
-	double e = pow(10, -k); // эпсилон
-	double r2 {acos(-1) / 2}; // ряды Тейлора
-	double r1 {r2 - atan(x)}; // стандартная функция
+	long double e = pow(10, -k); // эпсилон
+	long double r2 {acos(-1) / 2}; // ряды Тейлора
+	long double r1 {r2 - atan(x)}; // стандартная функция
 	int sign{1}, exp{-1};
-	while (true) {
+	long double term = -x;
+	do {
+		r2 += term;
 		sign *= -1;
 		exp += 2;
-		double t = 1.*(sign * pow(x, exp)) / exp;
-		if (fabs(t) < e) {
-			break;
-		}
-		r2 += t;
-	}
-	double delta = fabs(r2 - r1);
+		term *= 1.*sign*(x*x)*(exp-2) / exp;
+	} while (fabs(term) < e);
+	long double delta = fabs(r2 - r1);
 	std::cout.precision(prec);
 	std::cout << "Result using basic functions: " << r1 << std::endl;
 	std::cout << "Result using Taylor rows: " << r2 << std::endl;
